cd37eae15b0abd32f710aeb6709994781b8aea8b
Akash Vaishnav <aakashrankawat19@gmail.com>
2023-11-02 17:38:57 +0530

    update

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 """To install lacal packeges we can use this setup code."""
+
 setup(
     name = "Diamond_price_prediction",
     version = '0.0.1',
     author='Akash Vaishnav',
-    author_email='akash.vaishnav1908@gmail.com'
+    author_email='akash.vaishnav1908@gmail.com',
     packages=find_packages()
-
 )
