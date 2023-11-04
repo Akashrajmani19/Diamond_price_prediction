42627a17fa2db235f718e2df6089bfd0de00ff7e
Akash Vaishnav <aakashrankawat19@gmail.com>
2023-11-04 17:31:14 +0530

    upfate data trasnformation

--- src/Diamond_price_prediction/components/data_transformation.py
+++ src/Diamond_price_prediction/components/data_transformation.py
@@ -82,11 +82,19 @@ class DataTransformation:
             input_feature_train_arr = preprocessor_obj.fit_transform(input_feature_train_df)
             input_feature_test_arr = preprocessor_obj.transform(input_feature_test_df)
             logging.info("Applying preproccesing object on training abd testing datasets.")
+            
+            train_arr = np.c_[input_feature_train_arr,np.array(target_feature_train_df)]
+            test_arr = np.c_[input_feature_test_arr,np.array(target_feature_test_df)]
+
             save_object(
                 file_path = self.data_transformation_config.preprocessor_obj_file_path,
                 obj = preprocessor_obj
                 )
-            
+            logging.info("Preproccessing picklefile saved")
+            return(
+                train_arr,
+                test_arr
+            )
 
         except Exception as e:
             logging.info("Exception occured in the intiate_datatransformation")
